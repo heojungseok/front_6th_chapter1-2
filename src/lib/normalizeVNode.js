ece7665d0acbd78e033d34583df6a27583698f91
heojungseok <tjrwjdgj@gamail.com>
2025-07-15 17:06:47 +0900

    test: normalizeVNode 테스트 진행 중
    - 테스트 3,4 단계 적용 전

--- src/lib/normalizeVNode.js
+++ src/lib/normalizeVNode.js
@@ -1,3 +1,22 @@
+import { createVNode } from "./createVNode.js";
+/*
+ * 가상 DOM(Virtual DOM) 트리의 일관성과 예측 가능성을 보장
+ *
+ * */
 export function normalizeVNode(vNode) {
+  // 1
+  if (vNode === null || typeof vNode === "undefined" || vNode === false) {
+    vNode = "";
+  }
+  // 2
+  if (typeof vNode === "string" || typeof vNode === "number") {
+    vNode = String(vNode);
+  }
+  // 3
+  console.log("💻 vNode : ", vNode);
+  if (typeof vNode?.type === "function") {
+
+  }
+  createVNode();
   return vNode;
 }
