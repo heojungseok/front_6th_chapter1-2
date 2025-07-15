c8f67b1c1cd0601fb4734f578ca177c6f3d02c7f
heojungseok <tjrwjdgj@gamail.com>
2025-07-15 10:14:00 +0900

    refactor: createVNode 로직 간소화
    - getArrayDepth 제거

--- src/lib/createVNode.js
+++ src/lib/createVNode.js
@@ -3,26 +3,6 @@
  * 실제 DOM으로 렌더링 할 때 필요한 모든 정보를 담고 있어야 함
  * */
 export function createVNode(type, props, ...children) {
-  children = [
-    ...children.filter((child) => child !== null && child !== undefined && child !== false && child !== true),
-  ];
-  function getArrayDepth(array) {
-    if (!Array.isArray(array)) {
-      return 1;
-    }
-
-    let max = 1;
-    for (const arrayElement of array) {
-      const currentDepth = getArrayDepth(arrayElement);
-
-      if (currentDepth + 1 > max) {
-        max = currentDepth + 1;
-      }
-    }
-    return max;
-  }
-  const threshold = 2;
-  const depth = getArrayDepth(children);
-
-  return { type, props, children: children.flat(threshold < depth ? Infinity : depth) };
+  const falsey = [null, undefined, "", false];
+  return { type, props, children: children.flat(Infinity).filter((child) => !falsey.includes(child)) };
 }
